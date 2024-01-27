5ca292e4edc8d0159421503edb4075d17e690029
sudhanshuunilever <sudhanshu.pandey@unilever.com>
2024-01-27 22:54:16 +0530

    testing...

--- app.py
+++ app.py
@@ -5,18 +5,12 @@ from urllib.parse import quote
 
 def create_app():
     app = Flask(__name__)
-    x=10
-    y=10000
-    z=38383
 
     @app.route('/')
     def home():
         return 'GFG 27th jan pipeline run 123'
 
     return app
-
-def a():
-    a()
 if __name__ == '__main__':
     app = create_app()
     app.run(host='0.0.0.0', port=80, debug=True)
