43859ce6cff0e450ab90fa1f64b03c9a90c5a123
sudhanshuunilever <sudhanshu.pandey@unilever.com>
2024-01-27 22:43:34 +0530

    new changes

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ from urllib.parse import quote
 
 def create_app():
     app = Flask(__name__)
+    x=10
+    y=10000
+    z=38383
 
     @app.route('/')
     def home():
@@ -12,6 +15,8 @@ def create_app():
 
     return app
 
+def a():
+    a()
 if __name__ == '__main__':
     app = create_app()
     app.run(host='0.0.0.0', port=80, debug=True)
