a0f08e6730498921b59e6fb064d02117ac9d12ae
sudhanshuunilever <sudhanshu.pandey@unilever.com>
2024-01-27 20:52:02 +0530

    updated 27th jan code in app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def create_app():
 
     @app.route('/')
     def home():
-        return 'GFG Updated Website Build Jenkins Pipeline'
+        return 'GFG Updated Website Build Jenkins Pipeline on 27th Jan'
 
     return app
 
