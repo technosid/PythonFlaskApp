70e3e5c0a9925ddd8c95281e409365717282fe3e
sudhanshuunilever <sudhanshu.pandey@unilever.com>
2024-01-28 18:27:04 +0530

    new changes

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def create_app():
 
     @app.route('/')
     def home():
-        return 'GFG 27th jan pipeline run 123456'
+        return 'GFG 27th jan pipeline run 12345678'
 
     return app
 if __name__ == '__main__':
