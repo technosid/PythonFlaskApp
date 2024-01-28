96c25e9add6758b4bc48fbf554eb25d8647e15bc
sudhanshuunilever <sudhanshu.pandey@unilever.com>
2024-01-28 21:22:48 +0530

    new

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def create_app():
 
     @app.route('/')
     def home():
-        return 'Wow CI-CD Pipeline is been developed'
+        return 'Wow CI-CD Pipeline is been developed..'
 
     return app
 if __name__ == '__main__':
